cfe358aaa4c3246a3ecd054b014ad4039d742d65
Michael Nissim <info@israelunique.com>
2013-08-12 22:59:59 +0300

    Improving project setup modal

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,4 +6,16 @@ module ProjectsHelper
   def hours_of project
     "#{project.duration_in_hours.round} hrs (expected #{project.hours_expected.round})"
   end
+  
+  def link_to_modal_form project
+    link_to "##{ idify( project ) }", :class => 'btn btn-mini btn-warning',
+        :'data-toggle' => "modal", :role=>"button" do
+      content_tag( :i, nil, :class => "icon-wrench" ) +
+      " Setup your project"
+    end
+  end
+  
+  def idify project
+    project.title.parameterize.camelize(:lower)
+  end
 end
