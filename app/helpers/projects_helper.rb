c7bb843e8b9fd5382550c6a3680fa539710fd736
Michael Nissim <info@israelunique.com>
2013-09-12 11:57:21 +0200

    minor fix in percentage display

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -50,7 +50,7 @@ module ProjectsHelper
 
     if milestone and finish
       "#{ milestone } of #{ finish } - " <<
-      "#{ project.relative_progress.to_i } " <<
+      "#{ project.relative_progress.to_i }% " <<
       "(#{ last_earned( project ) })"
     end
   end
